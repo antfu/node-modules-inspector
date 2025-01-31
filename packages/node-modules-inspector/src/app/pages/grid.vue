c5c21d60c8b43b1d3215bea241ba1441662dcbae
Anthony Fu <github@antfu.me>
2025-02-01 02:32:56 +0800

    wip: basic search

--- packages/node-modules-inspector/src/app/pages/grid.vue
+++ packages/node-modules-inspector/src/app/pages/grid.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import { packageData } from '~/state/data'
+import { filteredPackages } from '~/state/search'
 </script>
 
 <template>
   <div grid="~ cols-minmax-400px gap-2" pt-14 pl-100>
     <PackageInfoGrid
-      v-for="pkg of packageData?.packages.values()"
+      v-for="pkg of filteredPackages"
       :key="pkg.spec"
       :pkg="pkg"
     />
--- packages/node-modules-inspector/src/app/state/search.ts
+++ packages/node-modules-inspector/src/app/state/search.ts
@@ -1,5 +1,7 @@
 import type { PackageModuleType } from 'node-modules-tools'
-import { reactive } from 'vue'
+import { useDebounce } from '@vueuse/core'
+import { computed, reactive } from 'vue'
+import { packageData } from './data'
 
 export interface FilterOptions {
   search: string
@@ -12,3 +14,17 @@ export const filters = reactive<FilterOptions>({
   module: null,
   license: null,
 })
+
+const debouncedSearch = useDebounce(computed(() => filters.search), 200)
+
+export const filteredPackages = computed(() => Array.from((function *() {
+  for (const pkg of packageData.value?.packages.values() || []) {
+    if (filters.module && pkg.resolved.module !== filters.module)
+      continue
+    if (filters.license && pkg.resolved.license !== filters.license)
+      continue
+    if (debouncedSearch.value && !pkg.name.includes(debouncedSearch.value))
+      continue
+    yield pkg
+  }
+})()))
