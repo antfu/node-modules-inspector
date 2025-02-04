e4b04ef44eb2f9787f9294e933d82c7c3f6733c0
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-02-05 00:01:18 +0800

    feat: load more transitive dependencies

--- packages/node-modules-inspector/src/app/components/report/MostDeps.vue
+++ packages/node-modules-inspector/src/app/components/report/MostDeps.vue
@@ -1,35 +1,47 @@
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import { selectedNode } from '~/state/current'
 import { payload } from '~/state/payload'
 
-const top20 = computed(() => {
-  return Array.from(payload.filtered.packages)
-    .filter(x => !x.workspace)
-    .sort((a, b) => b.flatDependencies.size - a.flatDependencies.size)
-    .slice(0, 20)
-})
+const count = ref(20)
+
+const transitiveDeps = computed(() =>
+  Array.from(payload.filtered.packages)
+    .filter(x => !x.workspace && x.flatDependencies.size > 0)
+    .sort((a, b) => b.flatDependencies.size - a.flatDependencies.size),
+)
+const top = computed(() => transitiveDeps.value.slice(0, count.value))
 </script>
 
 <template>
-  <div v-if="top20.length">
-    <SubTitle>Packages with the Most of Transitive Dependencies</SubTitle>
-    <div grid="~ cols-[max-content_max-content_1fr] gap-x-4 gap-y-1" border="~ rounded-xl base" p4 bg-glass of-auto>
-      <template v-for="pkg of top20" :key="pkg.spec">
-        <button
-          font-mono text-left hover:bg-active px2 ml--2 rounded
-          @click="selectedNode = pkg"
-        >
-          <DisplayPackageSpec :pkg />
-        </button>
-        <div flex="~ justify-end items-center">
-          <DisplayNumberBadge
-            :number="pkg.flatDependencies.size"
-            rounded-full text-sm h-max
-          />
-        </div>
-        <ModuleTypePercentage :pkg="pkg" :flat="true" />
-      </template>
+  <div v-if="top.length">
+    <SubTitle>
+      Packages with the Most of Transitive Dependencies
+      <DisplayNumberBadge :number="transitiveDeps.length" rounded-full text-sm />
+    </SubTitle>
+    <div flex flex-col gap2 border="~ rounded-xl base" p4 bg-glass of-auto>
+      <div grid="~ cols-[max-content_max-content_1fr] gap-x-4 gap-y-1">
+        <template v-for="pkg of top" :key="pkg.spec">
+          <button
+            font-mono text-left hover:bg-active px2 ml--2 rounded
+            @click="selectedNode = pkg"
+          >
+            <DisplayPackageSpec :pkg />
+          </button>
+          <div flex="~ justify-end items-center">
+            <DisplayNumberBadge
+              :number="pkg.flatDependencies.size"
+              rounded-full text-sm h-max
+            />
+          </div>
+          <ModuleTypePercentage :pkg="pkg" :flat="true" />
+        </template>
+      </div>
+
+      <button v-if="transitiveDeps.length > count" flex gap1 items-center self-end hover:bg-active px4 py2 rounded-full @click="count *= 1.5">
+        <div class="i-ri:arrow-down-double-line" />
+        More
+      </button>
     </div>
   </div>
 </template>
