8c1b648881169150bc0790556e5dba8d4896d90c
Kevin Deng 三咲智子 <sxzz@sxzz.moe>
2025-02-05 16:52:47 +0800

    feat: negative filter (#15)

--- packages/node-modules-inspector/src/app/state/payload.ts
+++ packages/node-modules-inspector/src/app/state/payload.ts
@@ -143,13 +143,24 @@ const _filtered = createComputedPayload(() => Array.from((function *() {
     }
 
     if (filterSearchDebounced.value) {
-      if (filterSearchDebounced.value.match(/[*[\]]/)) {
-        if (!pm.isMatch(pkg.name, filterSearchDebounced.value))
-          continue
+      let search = filterSearchDebounced.value
+      let filtered = false
+
+      const negative = search.startsWith('!')
+      if (negative)
+        search = search.slice(1)
+
+      if (search.match(/[*[\]]/)) {
+        if (!pm.isMatch(pkg.name, search))
+          filtered = true
       }
       else {
-        if (!pkg.name.includes(filterSearchDebounced.value))
-          continue
+        if (!pkg.name.includes(search))
+          filtered = true
+      }
+
+      if (negative ? !filtered : filtered) {
+        continue
       }
     }
 
