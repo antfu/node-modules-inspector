c9c4e8c1f0e8e4425d6015c368ba60244d6e46ff
Anthony Fu <github@antfu.me>
2025-02-06 15:13:59 +0800

    chore: update

--- packages/node-modules-inspector/src/app/utils/search-parser.test.ts
+++ packages/node-modules-inspector/src/app/utils/search-parser.test.ts
@@ -8,22 +8,22 @@ it('basic', () => {
 
 it('author simple', () => {
   expect(parseSearch('author:antfu'))
-    .toEqual({ text: '', author: [/^antfu$/gi] })
+    .toEqual({ text: '', author: [/antfu/gi] })
   expect(parseSearch('author:'))
     .toEqual({ text: '' })
   expect(parseSearch('Hello author:antfu'))
-    .toEqual({ text: 'Hello', author: [/^antfu$/gi] })
+    .toEqual({ text: 'Hello', author: [/antfu/gi] })
   expect(parseSearch('Hello author:antfu World author:ilyaliao'))
-    .toEqual({ text: 'Hello World', author: [/^antfu$/gi, /^ilyaliao$/gi] })
+    .toEqual({ text: 'Hello World', author: [/antfu/gi, /ilyaliao/gi] })
 })
 
 it('author with quote', () => {
   expect(parseSearch('author:"Anthony Fu"'))
-    .toEqual({ text: '', author: [/^Anthony Fu$/gi] })
+    .toEqual({ text: '', author: [/Anthony Fu/gi] })
   expect(parseSearch('author:""'))
     .toEqual({ text: '' })
   expect(parseSearch(`Hello author:\`Anthony Fu\` World author:"Ilya Liao"`))
-    .toEqual({ text: 'Hello World', author: [/^Anthony Fu$/gi, /^Ilya Liao$/gi] })
+    .toEqual({ text: 'Hello World', author: [/Anthony Fu/gi, /Ilya Liao/gi] })
 })
 
 it('mixed fields', () => {
@@ -31,8 +31,8 @@ it('mixed fields', () => {
     .toEqual({
       invert: true,
       text: 'Some name',
-      license: [/^MIT$/gi, /^Apache-2\.0$/gi],
-      author: [/^Anthony Fu$/gi],
-      not: [/^foo$/gi],
+      license: [/MIT/gi, /Apache-2\.0/gi],
+      author: [/Anthony Fu/gi],
+      not: [/foo/gi],
     })
 })
