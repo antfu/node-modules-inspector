71cacbb2109648337cebbfca8ef103b0cc2f87a9
Anthony Fu <github@antfu.me>
2025-03-08 16:19:56 +0800

    Discard changes to packages/node-modules-tools/src/index.ts

--- packages/node-modules-tools/src/index.ts
+++ packages/node-modules-tools/src/index.ts
@@ -1,7 +1,6 @@
 export * from './agent-entry'
 export * from './constants'
 
-export { getPackageDeprecatedInfo } from './deprecated'
 export { listPackageDependencies } from './list'
 export { resolvePackage } from './resolve'
 
