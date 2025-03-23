5145540a8329b5981a108bd7f189bf3052dbe01d
userquin <userquin@gmail.com>
2025-03-23 01:14:01 +0100

    chore: fix typecheck script

--- packages/node-modules-inspector/src/app/modules/webcontainer.ts
+++ packages/node-modules-inspector/src/app/modules/webcontainer.ts
@@ -6,15 +6,21 @@ export default defineNuxtModule({
   meta: {
     name: 'webcontainer-setup',
   },
-  setup(_, nuxt) {
-    if (!nuxt.options._prepare) {
-      addTemplate({
-        filename: 'webcontainer-server-code',
-        getContents: async () => {
+  setup() {
+    addTemplate({
+      filename: 'webcontainer-server-code',
+      getContents: async ({ nuxt }) => {
+        try {
           const content = await fs.readFile(fileURLToPath(new URL('../../../runtime/webcontainer-server.mjs', import.meta.url)), 'utf-8')
           return `export const WEBCONTAINER_SERVER_CODE = ${JSON.stringify(content)}`
-        },
-      })
-    }
+        }
+        catch (e) {
+          if (nuxt.options._prepare) {
+            return `export const WEBCONTAINER_SERVER_CODE = ''`
+          }
+          throw e
+        }
+      },
+    })
   },
 })
